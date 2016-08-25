a253ab74ac96fe0e911d4f6e40f6383059a15f0d
Theo Belaire <theo.belaire@gmail.com>
2016-08-25 11:02:07 -0400

    Parsing and Displaying Mazes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
+use std::fmt;
 
+#[derive(Debug)]
 enum Tile {
     Floor,
     Wall,
     Exit
 }
 
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match *self {
+            Tile::Floor => ' ',
+            Tile::Wall => '#',
+            Tile::Exit => 'X',
+        })
+    }
+}
+
+#[derive(Debug)]
 struct Maze {
     map: Vec<Vec<Tile>>
 }
 
+impl fmt::Display for Maze {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for ref line in &self.map {
+            for t in line.iter() {
+                try!(write!(f, "{}", t))
+            }
+            try!(writeln!(f, ""))
+        }
+        Ok(())
+    }
+}
+
 fn read_maze(filename: &str) -> std::io::Result<Maze> {
-    let mut f = try!(File::open(filename));
+    let f = try!(File::open(filename));
     let mut reader = BufReader::new(f);
     let mut maze = Maze{ map: Vec::new() };
     loop {
@@ -22,7 +47,13 @@ fn read_maze(filename: &str) -> std::io::Result<Maze> {
         if size == 0 {
             break;
         }
-        println!("line: {}", line);
+        let maze_line = line[.. size -1 ].chars().map(|c| match c {
+            '#' => Tile::Wall,
+            ' ' => Tile::Floor,
+            'X' => Tile::Exit,
+            _   => panic!("Bad maze character '{}'", c)
+        }).collect();
+        maze.map.push(maze_line);
     }
     return Ok(maze);
 }
@@ -31,6 +62,6 @@ fn main() {
     let maze = read_maze("maze.txt");
     match maze {
         Err(err) => println!("err is {:?}", err),
-        Ok(maze) => println!("got maze"),
+        Ok(maze) => println!("got maze\n{}", maze),
     }
 }
