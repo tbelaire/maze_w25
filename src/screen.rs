50857d4feb38147213ee9b242a7fd0f71bc3057b
Theo Belaire <theo.belaire@gmail.com>
2016-08-28 18:47:28 -0400

    Add reminder about viewport

--- src/screen.rs
+++ src/screen.rs
@@ -1,5 +1,6 @@
 
 // The screen in 1 indexed, not zero indexed.
 pub fn move_cursor(row: usize, col: usize) {
+    // TODO add a camera/viewport
     print!("\x1B[{row};{col}f", row = 1 + row, col = 1 + col);
 }
