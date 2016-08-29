4f71f5b5506ba46b050970e1c6f05e710b69b646
Theo Belaire <theo.belaire@gmail.com>
2016-08-28 20:14:59 -0400

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -50,13 +50,16 @@ fn main() {
     let mut maze = Maze::from_file("maze.txt").unwrap();
 
     let logger_config = fern::DispatchConfig {
-    format: Box::new(|msg: &str, level: &log::LogLevel, _location: &log::LogLocation| {
-        // This is a fairly simple format, though it's possible to do more complicated ones.
-        // This closure can contain any code, as long as it produces a String message.
-        format!("[{}][{}] {}", time::now().strftime("%Y-%m-%d][%H:%M:%S").unwrap(), level, msg)
-    }),
-    output: vec![fern::OutputConfig::file("maze.log")],
-    level: log::LogLevelFilter::Trace,
+        format: Box::new(|msg: &str, level: &log::LogLevel, _location: &log::LogLocation| {
+            // This is a fairly simple format, though it's possible to do more complicated ones.
+            // This closure can contain any code, as long as it produces a String message.
+            format!("[{}][{}] {}",
+                    time::now().strftime("%Y-%m-%d][%H:%M:%S").unwrap(),
+                    level,
+                    msg)
+        }),
+        output: vec![fern::OutputConfig::file("maze.log")],
+        level: log::LogLevelFilter::Trace,
     };
     if let Err(e) = fern::init_global_logger(logger_config, log::LogLevelFilter::Trace) {
         panic!("Failed to initialize global logger: {}", e);
